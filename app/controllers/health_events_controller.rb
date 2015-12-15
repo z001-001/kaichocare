d835e61ddb74a2549854ed4e8b66ccae5f6825e1
z001_001 <z001-001@drmscp.net>
2015-12-15 14:44:03 +0000

    Add posting health event

---
+++ app/controllers/health_events_controller.rb
@@ -0,0 +1,50 @@
+class HealthEventsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_health_event, only: [:destroy, :edit, :update]
+
+  def new
+    @health_event = current_user.health_events.build
+  end
+
+  def create
+    @health_event = current_user.health_events.build(health_event_params)
+    if @health_event.save
+      flash[:success] = "Health event created!"
+      redirect_to current_user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @health_event.update(health_event_params)
+      # 保存に成功した場合はトップページにリダイレクト
+      flash[:success] = "Health event updated!"
+      redirect_to current_user
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
+  def destroy
+    #@health_event = current_user.health_events.find_by(id: params[:id])
+    return redirect_to root_url if @health_event.nil?
+    @health_event.destroy
+    flash[:success] = "Health event deleted"
+    redirect_to request.referrer || root_url
+  end
+
+  private
+  def health_event_params
+    params.require(:health_event).permit(:title, :comment, :feeling,
+            :occurred_at, :ended_at, :public_level)
+  end
+
+  def set_health_event
+    @health_event = current_user.health_events.find_by(id: params[:id])
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @share_posts = @user.share_posts.order(created_at: :desc)
+    @health_events = @user.health_events.order(created_at: :desc)
   end
 end
