798245ad9e92a72d41b241deaa62b00e2720253e
z001_001 <z001-001@drmscp.net>
2016-01-29 09:17:52 +0000

    Adjust list display

--- app/models/bowel.rb
+++ app/models/bowel.rb
@@ -16,8 +16,22 @@ class Bowel < ActiveRecord::Base
     lumpy: 2,           # 硬い便
     cracky_sausage: 3,  # やや硬い便
     normal: 4,          # 普通便
-    soft_blobs: 5,      # やや軟らかい便
+    soft_blobs: 5,      # やや軟便
     mushy: 6,           # 泥状便
     liquid: 7,          # 水様便
   }
+
+  def self.shape_name(shape)
+    case shape
+    when "unknown", 0        then "不明"
+    when "hard_lumps", 1     then "コロコロ便"
+    when "lumpy", 2          then "硬い便"
+    when "cracky_sausage", 3 then "やや硬い便"
+    when "normal", 4         then "普通便"
+    when "soft_blobs", 5     then "やや軟便"
+    when "mushy", 6          then "泥状便"
+    when "liquid", 7         then "水様便"
+    else                          ""
+    end
+  end
 end
