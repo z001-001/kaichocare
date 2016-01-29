244c0651ed358644258fb2a2c15b472c17b80af7
z001_001 <z001-001@drmscp.net>
2016-01-29 08:21:12 +0000

    Add help page

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
 
   get 'search', to: 'search#index'
 
+  get 'static_pages/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
