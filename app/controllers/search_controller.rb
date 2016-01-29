1a3aa2afb6a4535f59ce045cf8347e02c4fdefb9
z001_001 <z001-001@drmscp.net>
2016-01-29 04:25:36 +0000

    Add search feature

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,35 +1,44 @@
-class WelcomeController < ApplicationController
+class SearchController < ApplicationController
   include PublicLevelHelper
+  before_action :authenticate_user!
 
   def index
-    if user_signed_in?
+    if params[:q].present?
+      @q = params[:q]
       @user = current_user
       bowels = Bowel.where("(user_id in (?) AND public_level >= ?) OR " +
                             "(user_id in (#{@user.friend_user_ids.join(',')}) AND public_level >= ?) OR " +
                             "(user_id in (#{@user.following_user_ids.join(',')}) AND public_level >= ?)",
                             @user.id, LEVEL_DEFAULT,
                             LEVEL_FRIEND_SUMMARY,
-                            LEVEL_PUBLIC_SUMMARY)\
-                    .order(created_at: :desc).limit(10)
+                            LEVEL_PUBLIC_SUMMARY)
+      bowels = bowels.where("comment LIKE ?", "%#{@q}%")
+      
       health_events = HealthEvent.where("(user_id in (?) AND public_level >= ?) OR " +
                             "(user_id in (#{@user.friend_user_ids.join(',')}) AND public_level >= ?) OR " +
                             "(user_id in (#{@user.following_user_ids.join(',')}) AND public_level >= ?)",
                             @user.id, LEVEL_DEFAULT,
                             LEVEL_FRIEND_SUMMARY,
-                            LEVEL_PUBLIC_SUMMARY)\
-                     .order(created_at: :desc).limit(10)
-      @feed_items = bowels + health_events
-      @feed_items = @feed_items.sort_by { |item| item.occurred_at }.reverse.slice(0, 10)
+                            LEVEL_PUBLIC_SUMMARY)
+      health_events = health_events.where("(title LIKE ?) OR (comment LIKE ?)",
+                            "%#{@q}%", "%#{@q}%")
 
-      @share_posts = SharePost.where("(user_id in (?) AND public_level >= ?) OR " +
+      share_posts = SharePost.where("(user_id in (?) AND public_level >= ?) OR " +
                             "(user_id in (#{@user.friend_user_ids.join(',')}) AND public_level >= ?) OR " +
                             "(public_level >= ?)",
                             @user.id, LEVEL_DEFAULT,
                             LEVEL_FRIEND_SUMMARY,
-                            LEVEL_PUBLIC_SUMMARY)\
-                     .order(created_at: :desc).limit(10)
+                            LEVEL_PUBLIC_SUMMARY)
+      share_posts = share_posts.where("(title LIKE ?) OR (comment LIKE ?)",
+                            "%#{@q}%", "%#{@q}%")
 
-      @friend_users = @user.friend_users.limit(3).shuffle
+
+      result_array = bowels + health_events + share_posts
+      result_array = result_array.sort_by { |item| item.updated_at }.reverse
+      @feed_items = Kaminari.paginate_array(result_array).page(params[:page])
+    else
+      redirect_to request.referrer || root_url
     end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   resources :bowels, except: [:index]
   resources :relationships, only: [:create, :destroy]
 
+  get 'search', to: 'search#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
